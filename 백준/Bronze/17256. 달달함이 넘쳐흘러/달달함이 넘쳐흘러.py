d4870daba5c259f0a8830130c85d7885e0ae1877
TraceofLight <98262849+TraceofLight@users.noreply.github.com>
2023-02-12 11:06:15 +0900

    [Bronze V] Title: 달달함이 넘쳐흘러, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.py
@@ -0,0 +1,14 @@
+# 달달함이 넘쳐흘러
+
+import sys
+
+input = sys.stdin.readline
+
+ax, ay, az = map(int, input().split())
+cx, cy, cz = map(int, input().split())
+
+bx = cx - az
+by = cy // ay
+bz = cz - ax
+
+print(bx, by, bz)
