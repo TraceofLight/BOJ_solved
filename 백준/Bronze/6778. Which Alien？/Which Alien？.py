343275220eceb577ed595e1c035d5fac9bdf953d
TraceofLight <98262849+TraceofLight@users.noreply.github.com>
2023-04-20 17:39:20 +0900

    [Bronze IV] Title: Which Alien?, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.py
@@ -0,0 +1,11 @@
+# Which Alien?
+
+antenna = int(input())
+eye = int(input())
+
+if antenna >= 3 and eye <= 4:
+    print('TroyMartian')
+if antenna <= 6 and eye >= 2:
+    print('VladSaturnian')
+if antenna <= 2 and eye <= 3:
+    print('GraemeMercurian')
