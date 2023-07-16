add30a27e4000c1dc4dd0a32013d0a50c35b7479
TraceofLight <98262849+TraceofLight@users.noreply.github.com>
2023-07-17 02:31:26 +0900

    [Bronze IV] Title: Telemarketer or not?, Time: 128 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/16017. Telemarketer or not？/Telemarketer or not？.py
@@ -0,0 +1,14 @@
+# Telemarketer or not?
+
+number = []
+for _ in range(4):
+    number.append(int(input()))
+    
+if (
+    (number[0] == 8 or number[0] == 9) and 
+    (number[3] == 8 or number[3] == 9) and 
+    (number[1] == number[2])
+):
+    print('ignore')
+else:
+    print('answer')
