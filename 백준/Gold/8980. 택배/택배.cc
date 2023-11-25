a93f8c1362d634107cf0f60155dc56cc38477dad
TraceofLight <98262849+TraceofLight@users.noreply.github.com>
2023-11-26 01:20:00 +0900

    [Gold I] Title: 택배, Time: 12 ms, Memory: 2532 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8980. 택배/택배.cc
+++ 백준/Gold/8980. 택배/택배.cc
@@ -1,7 +1,7 @@
 // 택배
 
 #include <iostream>
-#include <queue>
+#include <algorithm>
 #include <vector>
 
 using namespace std;
@@ -9,18 +9,30 @@ using namespace std;
 struct Parcel {
   int start;
   int end;
-  int weight;
   int total_box;
 
   Parcel(int s, int e, int box)
-      : start(s), end(e), total_box(box) {
-    weight = end - start;
+      : start(s), end(e), total_box(box) {}
+
+  Parcel(Parcel &&other) noexcept
+      : start(other.start),
+        end(other.end),
+        total_box(other.total_box) {
+  }
+
+  Parcel &operator=(Parcel &&other) noexcept {
+    if (this != &other) {
+      start = other.start;
+      end = other.end;
+      total_box = other.total_box;
+    }
+    return *this;
   }
 };
 
 struct Compare {
   bool operator()(const Parcel &first, const Parcel &second) {
-    return first.weight > second.weight;
+    return first.end < second.end;
   }
 };
 
@@ -48,22 +60,28 @@ int main() {
   int query_number;
   cin >> query_number;
 
-  priority_queue<Parcel, vector<Parcel>, Compare> parcel_pq;
+  vector<Parcel> parcel_vector;
   int start, end, parcel_box;
   for (int i = 0; i < query_number; i++) {
     cin >> start >> end >> parcel_box;
-    parcel_pq.push(Parcel(start, end, parcel_box));
+    Parcel input_parcel_info = Parcel(start, end, parcel_box);
+    parcel_vector.push_back(std::move(input_parcel_info));
   }
+  sort(parcel_vector.begin(), parcel_vector.end(), Compare());
 
-  vector<int> capacity_arr(town_number + 1, truck_capacity);
+  vector<int> capacity_arr(town_number, truck_capacity);
   int result = 0;
 
-  while (!parcel_pq.empty()) {
-    int max_load = FindMaxLoad(capacity_arr, parcel_pq.top().start, parcel_pq.top().end);
-    int now_load = min(max_load, parcel_pq.top().total_box);
+  for (Parcel &each_parcel : parcel_vector) {
+    int max_load =
+        FindMaxLoad(capacity_arr, each_parcel.start, each_parcel.end);
+    int now_load = min(max_load, each_parcel.total_box);
     result += now_load;
-    UpdateLoad(capacity_arr, parcel_pq.top().start, parcel_pq.top().end, now_load);
-    parcel_pq.pop();
+    UpdateLoad(capacity_arr,
+               each_parcel.start,
+               each_parcel.end,
+               now_load);
   }
+
   cout << result << endl;
 }
